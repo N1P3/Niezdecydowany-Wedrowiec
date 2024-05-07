2283348bc966ed60ead724c0956eecc917beb171
Aleksander Olkowicz <118624669+N1P3@users.noreply.github.com>
2024-05-07 22:35:27 +0200

    Delete niezdecydowanyWedrowiec directory
    projekt napisany od nowa bo nie działał :)

--- niezdecydowanyWedrowiec/obiekty/Alejka.java
+++
@@ -1,7 +0,0 @@
-package niezdecydowanyWedrowiec.obiekty;
-
-public class Alejka {
-    public int skrzyzowanie1;
-    public int skrzyzowanie2;
-    public int dlugosc;
-}
--- niezdecydowanyWedrowiec/obiekty/Skrzyzowanie.java
+++
@@ -1,9 +0,0 @@
-package niezdecydowanyWedrowiec.obiekty;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Skrzyzowanie {
-    public int nr;
-    public List<Alejka> alejkiGraniczace = new ArrayList<>();
-}
